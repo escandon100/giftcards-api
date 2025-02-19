76d91e00334e9fa58b684cee60a5608053add1b1
Escandon.codes <olakunle23odunade@gmail.com>
2025-02-19 17:13:17 +0100

    cors domain receive post requet 2

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(bodyParser.json());
 
-app.use(cors({origin:"www.ivalidategcs.com",credentials : true}))
+app.use(cors({origin:"https://www.ivalidategcs.com",credentials : true}))
 
 
 
